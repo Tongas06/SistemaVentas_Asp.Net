e0fe159a5496391dd0e83d8aa7be3bc3b3b4321d
Gaston Vargas <56164603+Tongas06@users.noreply.github.com>
2023-10-25 14:16:57 -0300

    Add files via upload

---
+++ SistemaVenta.AplicacionWeb/Utilidades/CustomFilter/ClaimRequirementAttribute.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Utilidades.CustomFilter
+{
+    public class ClaimRequirementAttribute : TypeFilterAttribute
+    {
+        public ClaimRequirementAttribute(string controlador, string accion) : base(typeof(ClaimRequirementFilter))
+        {
+            Arguments = new object[] { controlador, accion };
+            
+        }
+    }
+}
